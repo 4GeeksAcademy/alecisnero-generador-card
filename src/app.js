64125aaf422a4d9a70c8383828fb6b2b26875c98
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-03-16 02:01:47 +0000

    Se BUENAS PRACTICAS/ha cambiado nombre de español a ingles

--- src/app.js
+++ src/app.js
@@ -6,61 +6,64 @@ window.onload = function() {
 };
 
 //boton selecionado
-let boton = document.querySelector("#button");
+let button = document.querySelector("#button");
 
 //array que contienen los valores
 let numberCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "K", "Q"];
-let simbolos = ["♦", "♥", "♠", "♣"];
+let symbol = ["♦", "♥", "♠", "♣"];
 
-const carta = document.getElementById("sim");
-const carta2 = document.getElementById("sim2");
+const card = document.getElementById("symbol");
+const card2 = document.getElementById("symbol2");
 
 //funcion para generar el number
-const generarCard = () => Math.floor(Math.random() * 12);
-const generarSim = () => Math.floor(Math.random() * 4);
+const generateCard = () => Math.floor(Math.random() * 12); //indexCard 4
+const generateSymbol = () => Math.floor(Math.random() * 4);//indexSymbol 1
 
 //funcio para obtener el valor de las cartas y simbolos
-const getNumberCard = indCard => {
-  const numberRandomCard = numberCard[indCard];
+const getNumberCard = indexCard => {
+  const numberRandomCard = numberCard[indexCard]; //5
   return `${numberRandomCard}`;
 };
 
-const getSymbolCard = indSim => {
-  const numberRandomSim = simbolos[indSim];
-  return `${numberRandomSim}`;
+const getSymbolCard = indexSymbol => {
+  const numberRandomSymbol = symbol[indexSymbol]; //"♥"
+  return `${numberRandomSymbol}`;
 };
 
-const mostrarCard = (nomCard, symbols) => {
+const showCard = (nameCard, symbols) => {
   if (symbols === "♦" || symbols === "♥") {
-    const carta = document.getElementById("carta");
-    carta.textContent = nomCard;
-    carta.style.color = "red";
+    const card = document.getElementById("card");
+    card.textContent = nameCard;
+    card.style.color = "red";
   } else {
-    const carta = document.getElementById("carta");
-    carta.textContent = nomCard;
-    carta.style.color = "black";
+    const card = document.getElementById("card");
+    card.textContent = nameCard;
+    card.style.color = "black";
   }
 };
 
-const mostrarSym = simCard => {
-  if (simCard === "♦" || simCard === "♥") {
-    carta.textContent = simCard;
-    carta2.textContent = simCard;
-    carta.style.color = "red";
-    carta2.style.color = "red";
+const showSym = symbolCard => {
+  if (symbolCard === "♦" || symbolCard === "♥") {
+    card.textContent = symbolCard;
+    card2.textContent = symbolCard;
+    card.style.color = "red";
+    card2.style.color = "red";
   } else {
-    carta.textContent = simCard;
-    carta2.textContent = simCard;
-    carta.style.color = "black";
-    carta2.style.color = "black";
+    card.textContent = symbolCard;
+    card2.textContent = symbolCard;
+    card.style.color = "black";
+    card2.style.color = "black";
   }
 };
 
-boton.addEventListener("click", () => {
-  const numberGeneradoCard = generarCard();
-  const symbolGeneradoCard = generarSim();
-  const getNomCard = getNumberCard(numberGeneradoCard);
-  const getSimCard = getSymbolCard(symbolGeneradoCard);
-  mostrarCard(getNomCard, getSimCard);
-  mostrarSym(getSimCard);
+button.addEventListener("click", () => {
+  const numberGenerateCard = generateCard(); //indexCard 4
+  const symbolGenerateCard = generateSymbol();//indexSymbol 1
+
+  const resultNameCard = getNumberCard(numberGenerateCard); //5
+  const resultSymbolCard = getSymbolCard(symbolGenerateCard); //"♥"
+
+  showCard(resultNameCard, resultSymbolCard); //"♥"  si es rojo o negro
+                                          //5
+  showSym(resultSymbolCard);                //"♥"
 });
